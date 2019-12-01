9d7caaf2029e9cbc485f17cf71b7c5fb98fe0a25
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 17:52:23 +0100

    d

--- chapitre2/tracker.php
+++ chapitre2/tracker.php
@@ -8,7 +8,12 @@ $heightm = (float)($heightCm)/100;
 
 echo $heightm;
 $heigthsquare= $heightm*$heightm;
+echo '<br>';
 echo $heigthsquare;
+
+$bmi = $weightKg / $heigthsquare;
+echo '<br>';
+echo $bmi;
 
 
  ?>
