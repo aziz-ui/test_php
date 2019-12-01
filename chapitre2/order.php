371f30289e7b060cc6da13e746190759441dc09d
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 13:57:42 +0100

    a

---
+++ chapitre2/order.php
@@ -0,0 +1,26 @@
+<?php
+
+$str = 'Your order total is: ';
+
+$order = 20;
+
+$additional = 5;
+
+$orderTotal = 0;
+
+$complete = false;
+
+//add additional items to order
+
+$orderTotal = $order + $additional;
+
+//if order is equal to 25
+
+if ($orderTotal >= 25) {
+  $complete = true;
+
+//display the $str and add the orderTotal
+
+  echo $str . $orderTotal;
+};
+?>
