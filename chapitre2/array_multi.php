ebe3e3ea42401e9a0daeb41432468e99a8091f51
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 14:37:05 +0100

    a

---
+++ chapitre2/array_multi.php
@@ -0,0 +1,32 @@
+<?php
+
+ $heroInfo = [
+
+      [
+          'heroName' => 'Spiderman',
+
+          'weapon' => 'Spider web'
+
+      ],
+
+      [
+
+          'heroName' => 'Iron Man',
+
+          'weapon' => 'Mark L'
+
+     ],
+
+     [
+
+         'heroName' => 'Thor',
+
+         'weapon' => 'Mjolnir'
+
+     ],
+     echo '<pre>';
+
+     print_r($heroInfo);
+
+     echo '<pre>';
+?>
