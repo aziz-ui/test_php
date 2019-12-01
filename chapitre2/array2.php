92e8c98c81ebecc8e3ba632ed324afbd6be4a23a
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 14:06:43 +0100

    a

---
+++ chapitre2/array2.php
@@ -0,0 +1,17 @@
+<?php
+
+$heroInfo = array(
+
+    'name' => 'Peter Parker',
+
+    'superheroName' => 'Spiderman',
+
+    'city' => 'New York',
+
+    'creator' => 'Stan Lee'
+
+);
+
+print_r($heroInfo);
+
+?>
