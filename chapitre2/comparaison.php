c5b70d44e7714232bc5452f85731e8dc3cdc5fba
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 17:27:30 +0100

    a

---
+++ chapitre2/comparaison.php
@@ -0,0 +1,21 @@
+<?php
+
+$cost = 200;
+
+$money = 150;
+
+if ($cost == $money) {
+
+    echo 'cost matches money';
+
+}
+
+
+
+if ($cost != $money) {
+
+    echo 'cost does not match money';
+
+};
+
+?>
