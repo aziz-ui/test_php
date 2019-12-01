b375ba55f982ef167ce76f5366a65621c3e07171
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 13:32:58 +0100

    c

--- chapitre1/activity1.php
+++ chapitre1/activity1.php
@@ -1,29 +1,41 @@
-<!DOCTYPE html>
-
-<html lang="en">
-<head>
-
-    <title>Movie</title>
-
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-
-    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
-
-</head>
-<body>
-
 <?php
 
-  $movie = $_GET['movieName'];
-  $star = $_GET['movieStar'];
-  $year = $_Get['movieYear'];
+$name = $_GET['movieName'];
 
-  <h1> Information about $movie </h1>
+$star = $_GET['movieStar'];
 
-  <p> Based on the input so far here is the Information
-      $star stared in the movie $movie in the year $year
-  </p>
+$year = $_GET['movieYear'];
 
-  ?>
-</body>
-</html>
+?>
+
+   <head>
+
+          <meta charset="UTF-8">
+
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+         <title><?php echo $name; ?></title>
+
+     </head>
+
+     <body>
+
+         <div>
+
+             <h1>Information about <?php  echo $name; ?></h1>
+
+           <p>
+
+             Based on the input, here is the information so far:
+
+             <br>
+
+             <?php echo $star . ' starred in the movie ' . $name .' which was released in year ' . $year; ?>
+
+             </p>
+
+         </div>
+
+     </body>
