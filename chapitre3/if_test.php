177b886f21974e53b8bb8e64f4b349fa74a3cad8
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 21:32:18 +0100

    a

---
+++ chapitre3/if_test.php
@@ -0,0 +1,16 @@
+<?php
+$number1 = 3;
+
+$number2 = 5;
+
+if ($number1 > $number2) {
+
+    print("$number1 is greater than $ number2");
+
+} else {
+
+    print("$number1 is less than $number2"); //prints 3 is less than 5
+
+}
+
+ ?>
