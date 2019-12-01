10c52c8fa51a38e328ebffd7bb20d489d9bf4048
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 21:49:56 +0100

    a

---
+++ chapitre3/test_diff.php
@@ -0,0 +1,27 @@
+<?php
+
+$a = 5;
+
+$b = 3;
+
+if($a - $b) {
+
+    //placeholder for inner if...else
+    if ($a > $b) {
+
+        $difference = $a - $b;
+
+    } else {
+
+        $difference = $b - $a;
+
+    }
+
+print("The difference is $difference");
+} else {
+
+    print("The numbers are equal");
+
+}
+
+ ?>
