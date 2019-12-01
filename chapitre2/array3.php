badb7376a35f96176adb0d7647fc99f9727fe607
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-01 14:14:16 +0100

    a

---
+++ chapitre2/array3.php
@@ -0,0 +1,17 @@
+<?php
+
+$people = [];
+
+$people['Rose'] = 'Principal Software Architect';
+
+$people['Laura'] = 'Senior Software Architect';
+
+$people['Jane'] = 'Project Manager';
+
+$people['Mary'] = 'Software Architect;
+
+//display the contents of $people
+
+print_r($people);
+
+?>
