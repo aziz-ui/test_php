1410642a686db08515a66e088ab72a8b04c39bf9
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-02 11:12:08 +0100

    b

--- chapitre3/for_loop.php
+++ chapitre3/for_loop.php
@@ -3,7 +3,7 @@
 $days= ["Samedi","Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi"];
 $totaldays = count($days);
 for ($i=0; $i <$totaldays ; $i++) {
-  echo $days[i]." ";
+  echo $days[$i]." ";
 }
 
 ?>
