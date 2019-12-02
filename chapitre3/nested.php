09ee1170577e27ccb650672902cdc53dfa933cbb
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2019-12-02 14:44:08 +0100

    a

---
+++ chapitre3/nested.php
@@ -0,0 +1,9 @@
+<?php
+  $professions = ["Doctor", "Teacher", "Programmer", "Lawyer", "Athlete"];
+  $subjects =  ["Mathematics", "Computer Programming", "Business English", "Graph Theory"];
+  foreach ($professions as $profession) {
+
+        echo "The Profession is $profession. " . PHP_EOL;
+
+}
+ ?>
